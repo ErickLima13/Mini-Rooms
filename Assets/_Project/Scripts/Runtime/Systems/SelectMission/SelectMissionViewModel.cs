10b0e743d6abee79b405b20b240037d9fdf92cea
ErickAraujo <erickequipe7@gmail.com>
2026-07-03 14:50:56 -0300

    add : interface ichange scene

--- Assets/_Project/Scripts/Runtime/Systems/SelectMission/SelectMissionViewModel.cs
+++ Assets/_Project/Scripts/Runtime/Systems/SelectMission/SelectMissionViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using Cysharp.Threading.Tasks;
 using Maneuver.ScreenManager;
 using R3;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+using UnityEngine;
 
 namespace App.UI.Screens.Behaviour
 {
-
-
     public class SelectMissionViewModel : ScreenViewModelBase
     {
+
+        private IChangeScene _changeScene;
+
         public ReactiveProperty<int> MissionCount { get; } = new();
 
+        public ReactiveCommand<ScenesInGame> PlayLevelCommand { get; } = new();
+
+        public SelectMissionViewModel(IChangeScene changeScene)
+        {
+            _changeScene = changeScene;
+
+            TrackDisposable(PlayLevelCommand.Subscribe(scene => LoadLevel(scene)));
+        }
 
         public async UniTask GenerateCards()
         {
@@ -19,5 +27,11 @@ namespace App.UI.Screens.Behaviour
 
             MissionCount.Value = 5;
         }
+
+        private void LoadLevel(ScenesInGame scenes)
+        {
+            _changeScene.LoadScene(scenes);
+        }
+  
     }
 }
--- Assets/_Project/Scripts/Runtime/Utility/ChangeScene.cs
+++ Assets/_Project/Scripts/Runtime/Utility/ChangeScene.cs
@@ -4,19 +4,26 @@ using UnityEngine.SceneManagement;
 public enum ScenesInGame
 {
     MainMenu = 0,
-    Gameplay = 1
+    Gameplay = 1,
+    Level1,
+    Level2,
+    Level3,
+    Level4
+
 }
 
-public class ChangeScene : MonoBehaviour
+public class ChangeScene : IChangeScene
 {
     [SerializeField] private ScenesInGame _currentScene;
 
 
     public void LoadScene(ScenesInGame scenesInGame)
     {
-        SceneManager.LoadScene((int)scenesInGame);
+        //SceneManager.LoadScene((int)scenesInGame);
 
-        Scene currentScene = SceneManager.GetActiveScene();
-        _currentScene = (ScenesInGame)currentScene.buildIndex;
+        //Scene currentScene = SceneManager.GetActiveScene();
+        //_currentScene = (ScenesInGame)currentScene.buildIndex;
+
+        Debug.Log("chamei " + scenesInGame);
     }
 }
